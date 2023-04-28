088f4fbb0097b0f1c996bb20fe9cb542b21dc8d4
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-04-28 18:59:39 +0000

    navbar burgle toggle with a custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -22,43 +25,40 @@ const NavBar = () => {
 
   const addPostIcon = (
     <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
         className={styles.NavLink}
         activeClassName={styles.Active}
-        to="/posts/create"
-      >
-        <i className="fas fa-plus-square"></i>Add Post
-      </NavLink>
-  )
-  const loggedInIcons = <>
-      <NavLink
-          className={styles.NavLink}
-          activeClassName={styles.Active}
-          to="/feed"
+        to="/feed"
       >
         <i className="fas fa-stream"></i>Feed
       </NavLink>
       <NavLink
-          className={styles.NavLink}
-          activeClassName={styles.Active}
-          to="/liked"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
       >
         <i className="fas fa-heart"></i>Liked
       </NavLink>
-      <NavLink
-          className={styles.NavLink}
-          to="/"
-          onClick={handleSignOut}
-      >
-        <i className="fas fa-sign-out-alt"></i>Sign Out
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
       <NavLink
-          className={styles.NavLink}
-          to={`/profiles/${currentUser?.profile_id}`}
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
       >
         <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
       </NavLink>
-  
-  </>;
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -79,7 +79,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -87,7 +92,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
